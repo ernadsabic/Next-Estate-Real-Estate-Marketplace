b96f6d4d4d808b73cc0626eb9bb3915d0fe6e1e8
ernadsabic <ernadsabic923@gmail.com>
2026-08-12 22:28:08 +0200

    deployment fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
